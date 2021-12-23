a0cef3893a4077e736cb83a33dfbcf2a7c1d9ffe
Sergei <90544365+EvilhamsterZ@users.noreply.github.com>
2021-12-23 16:44:14 +0300

    Add files via upload

---
+++ vector/vector.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+void fill(std::vector<int>& v)//чет тут компилятор ругается на <>. убрал using namespace std// перестал ругаться
+{
+    for (int i = 0; i < v.size(); i++)
+    {
+        v[i] = i;
+    }
+    random_shuffle(v.begin(), v.end());//перемешиваем элементы вектора
+}
+
+void print(const std::vector<int>& v)
+{   
+    int j;
+    std::vector<int>::const_iterator largest = std::max_element(v.begin(), v.end());//в вектор втроена функция поиска макс элемента
+    std::cout << "Вывод массива перемешенного вектора \n";
+    for (int i = 0; i < v.size(); i++)
+    {
+        std::cout << v[i] << std::endl;
+    }
+    j= largest-v.begin();
+    std::cout << "Наибольший элемент вектора= "<<v[j];
+}
+int main()
+{   
+    setlocale(LC_ALL, "Rus");
+    int n;
+    std::cin >> n;
+    std::vector<int> v(n);
+    fill(v);
+    print(v);
+    return(0);
+}
